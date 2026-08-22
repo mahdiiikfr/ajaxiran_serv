da22586f958d72428025f4b8427dc8f41746fd0c
mahdiiikfr <mahdiii.kfr@gmail.com>
2026-08-22 09:04:01 -0700

    Update reply.py

--- src/keyboards/reply.py
+++ src/keyboards/reply.py
@@ -2,6 +2,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def get_main_menu() -> ReplyKeyboardMarkup:
     keyboard = [
+        [KeyboardButton(text="🎁 تست سرویس عادی (تانل/مستقیم)"), KeyboardButton(text="🎁 تست سرویس گیمینگ")],
         [KeyboardButton(text="🛒 خرید اشتراک عادی (تانل/مستقیم)"), KeyboardButton(text="🎮 خرید اشتراک گیمینگ")],
         [KeyboardButton(text="💼 خرید و مدیریت پنل نمایندگی")],
         [KeyboardButton(text="💰 کیف پول / شارژ"), KeyboardButton(text="📦 سرویس‌های من")],
